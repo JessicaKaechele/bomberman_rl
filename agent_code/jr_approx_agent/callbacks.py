2c5675588cab6c718960902d9a86ffde3eb1c9a1
Jonas Reinwald <jonas.reinwald@protonmail.com>
2021-03-25 21:08:51 +0100

    working agent for coins, modified for crates but not trained yet

--- agent_code/jr_approx_agent/callbacks.py
+++ agent_code/jr_approx_agent/callbacks.py
@@ -6,6 +6,8 @@ import os
 import numpy as np
 import pickle
 
+from sklearn.preprocessing import MinMaxScaler, PolynomialFeatures
+
 POSSIBLE_ACTIONS = ['UP', 'RIGHT', 'DOWN', 'LEFT', 'WAIT', 'BOMB']
 MODEL_FILE = 'weights.pt'
 
@@ -16,7 +18,7 @@ EXPLORATION_RATE_DECAY = 0.97
 
 def setup(self):
     self.is_fit = False
-    self.last_act_exploration = False
+    self.last_act_was_exploration = False
 
     if self.train:
         self.exploration_rate = EXPLORATION_RATE
@@ -47,7 +49,7 @@ def act(self, game_state: dict) -> str:
         # explore
         self.logger.debug("Exploring random action")
         action = np.random.choice(POSSIBLE_ACTIONS, p=[.2, .2, .2, .2, .1, .1])
-        self.last_act_exploration = True
+        self.last_act_was_exploration = True
     else:
         # exploit
         q = self.q if not self.train else self.q_a_predict
@@ -55,7 +57,7 @@ def act(self, game_state: dict) -> str:
 
         self.logger.debug("Exploiting (predict actions)")
         action = POSSIBLE_ACTIONS[np.argmax(q.predict(state_to_features(game_state).reshape(1, -1))[0])]
-        self.last_act_exploration = False
+        self.last_act_was_exploration = False
 
     self.exploration_rate *= EXPLORATION_RATE_DECAY
     self.exploration_rate = max(self.exploration_rate, 0.1)
@@ -73,47 +75,46 @@ def state_to_features(game_state):
     # others = game_state['others']
 
     field[self_x][self_y] = 5
-    walls_in_direction = [1 if field[self_x + 1][self_y] == -1 else 0,
-                          1 if field[self_x - 1][self_y] == -1 else 0,
-                          1 if field[self_x][self_y - 1] == -1 else 0,
-                          1 if field[self_x][self_y + 1] == -1 else 0]
-
-    # crate_in_direction = [1 if field[self_x + 1][self_y] == 1 else 0,
-    #                       1 if field[self_x - 1][self_y] == 1 else 0,
-    #                       1 if field[self_x][self_y - 1] == 1 else 0,
-    #                       1 if field[self_x][self_y + 1] == 1 else 0]
+    walls_and_crates_in_direction = [1 if field[self_x][self_y - 1] == -1 else 2 if field[self_x][self_y - 1] == 1 else 0,
+                                     1 if field[self_x + 1][self_y] == -1 else 2 if field[self_x + 1][self_y] == 1 else 0,
+                                     1 if field[self_x][self_y + 1] == -1 else 2 if field[self_x][self_y + 1] == 1 else 0,
+                                     1 if field[self_x - 1][self_y] == -1 else 2 if field[self_x - 1][self_y] == 1 else 0]
 
     [coin_x, coin_y], coin_dist = get_nearest_coin(game_state['coins'], (self_x, self_y))
-    coin_dir = get_dir(coin_x, coin_y, self_x, self_y)
+    _, coin_dir = get_dir(coin_x, coin_y, self_x, self_y)
     coin_dist_discrete = get_discrete_distance(coin_dist)
 
-    # [crate_x, crate_y], crate_dist = get_nearest_crate(field, (self_x, self_y))
-    # crate_dir = get_dir(crate_x, crate_y, self_x, self_y)
-    # crate_dist_discrete = get_discrete_distance(crate_dist)
-
     [bomb_x, bomb_y], bomb_dist = get_nearest_bomb(game_state['bombs'], (self_x, self_y))
-    bomb_dir = get_dir(bomb_x, bomb_y, self_x, self_y)
-    bomb_dist_discrete = get_discrete_distance(bomb_dist)
+    _, bomb_dir = get_dir(bomb_x, bomb_y, self_x, self_y)
+    # bomb_dist_discrete = get_discrete_distance(bomb_dist)
 
     [explosion_x, explosion_y], explosion_dist = get_nearest_explosion(game_state['explosion_map'], (self_x, self_y))
-    explosion_dir = get_dir(explosion_x, explosion_y, self_x, self_y)
-    explosion_dist_discrete = get_discrete_distance(explosion_dist)
+    _, explosion_dir = get_dir(explosion_x, explosion_y, self_x, self_y)
+    # explosion_dist_discrete = get_discrete_distance(explosion_dist)
+
+    [crate_x, crate_y], crate_dist = get_nearest_crate(field, (self_x, self_y))
+    _, crate_dir = get_dir(crate_x, crate_y, self_x, self_y)
+    # crate_dist_discrete = get_discrete_distance(crate_dist)
 
     can_lay_bomb = [1 if bombs_left else 0]
 
     # TODO: introduce some kind on non-linearity?
     # TODO: explosion distance not important, just don't run into it
     # TODO: inside blast radius to nearest bomb (all bombs?)
-    return np.array(walls_in_direction + coin_dir + bomb_dir + explosion_dir + coin_dist_discrete
-                    + bomb_dist_discrete + explosion_dist_discrete + can_lay_bomb)
+
+    min_maxed_data = MinMaxScaler().fit_transform(np.array(walls_and_crates_in_direction + coin_dir + bomb_dir + explosion_dir + crate_dir + can_lay_bomb).reshape(-1, 1))
+    features = PolynomialFeatures().fit_transform(min_maxed_data.T)
+
+    return features
 
 
 def get_nearest_coin(coin_map, self_pos):
-    min_dist = 5000
-    min_x, min_y = [-1, -1]
+    min_dist = None
+    min_x, min_y = None, None
+
     for x, y in coin_map:
         distance_to_agent = np.abs(x - self_pos[0]) + np.abs(y - self_pos[1])
-        if distance_to_agent < min_dist:
+        if min_dist is None or distance_to_agent < min_dist:
             min_dist = distance_to_agent
             min_x, min_y = x, y
 
@@ -121,30 +122,36 @@ def get_nearest_coin(coin_map, self_pos):
 
 
 def get_dir(x, y, self_x, self_y):
+    direction = [-1, -1, -1, -1]  # top, right, bottom, left
+
+    if x is None or y is None:
+        return False, direction
+
     pos_delta_x = x - self_x
     pos_delta_y = y - self_y
 
-    direction = [0, 0, 0, 0]  # top, right, bottom, left
-    if np.abs(pos_delta_x) >= np.abs(pos_delta_y):
-        # left or right
-        if pos_delta_x >= 0:
-            direction[1] = 1  # "RIGHT"
-        else:
-            direction[3] = 1   # "LEFT"
+    # left or right
+    if pos_delta_x > 0:
+        direction[1] = pos_delta_x  # "RIGHT"
+    elif pos_delta_x < 0:
+        direction[3] = -pos_delta_x   # "LEFT"
 
-    if np.abs(pos_delta_x) <= np.abs(pos_delta_y):
-        # top or bottom
-        if pos_delta_y >= 0:
-            direction[2] = 1   # "DOWN"
-        else:
-            direction[0] = 1   # "UP"
+    # top or bottom
+    if pos_delta_y > 0:
+        direction[2] = pos_delta_y   # "DOWN"
+    elif pos_delta_y < 0:
+        direction[0] = -pos_delta_y   # "UP"
 
-    return direction
+    return True, direction
 
 
 def get_discrete_distance(dist):
+    if dist is None or dist > 10:
+        return None
+
     distance = [0, 0, 0, 0]  # almost on top, near, middle, far
-    if dist > 4:
+
+    if dist > 3:
         distance[3] = 1
     elif dist > 2:
         distance[2] = 1
@@ -157,15 +164,15 @@ def get_discrete_distance(dist):
 
 
 def get_nearest_crate(field, self_pos):
-    min_dist = 5000
-    min_x, min_y = [-1, -1]
+    min_dist = None
+    min_x, min_y = None, None
 
     for [x, y], val in np.ndenumerate(field):
         if val != 1:
             continue
 
         distance_to_agent = np.abs(x - self_pos[0]) + np.abs(y - self_pos[1])
-        if distance_to_agent < min_dist:
+        if min_dist is None or distance_to_agent < min_dist:
             min_dist = distance_to_agent
             min_x, min_y = x, y
 
@@ -173,11 +180,12 @@ def get_nearest_crate(field, self_pos):
 
 
 def get_nearest_bomb(bombs, self_pos):
-    min_dist = 5000
-    min_x, min_y = [-1, -1]
+    min_dist = None
+    min_x, min_y = None, None
+
     for (x, y), _ in bombs:
         distance_to_agent = np.abs(x - self_pos[0]) + np.abs(y - self_pos[1])
-        if distance_to_agent < min_dist:
+        if min_dist is None or distance_to_agent < min_dist:
             min_dist = distance_to_agent
             min_x, min_y = x, y
 
@@ -185,15 +193,15 @@ def get_nearest_bomb(bombs, self_pos):
 
 
 def get_nearest_explosion(explosion_field, self_pos):
-    min_dist = 5000
-    min_x, min_y = [-1, -1]
+    min_dist = None
+    min_x, min_y = None, None
 
     for [x, y], val in np.ndenumerate(explosion_field):
         if val == 0:
             continue
 
         distance_to_agent = np.abs(x - self_pos[0]) + np.abs(y - self_pos[1])
-        if distance_to_agent < min_dist:
+        if min_dist is None or distance_to_agent < min_dist:
             min_dist = distance_to_agent
             min_x, min_y = x, y
 
--- agent_code/jr_approx_agent/train.py
+++ agent_code/jr_approx_agent/train.py
@@ -9,23 +9,22 @@ from sklearn.multioutput import MultiOutputRegressor
 from sklearn.preprocessing import StandardScaler
 
 import events as e
-from agent_code.double_q_approx_agent.callbacks import MODEL_FILE, POSSIBLE_ACTIONS
-from agent_code.double_q_approx_agent.custom_events import POTENTIAL_TO_COLLECT_COIN, POTENTIAL_TO_DIE_BY_BOMB, \
-    POTENTIAL_TO_DIE_BY_EXPLOSION
-from agent_code.double_q_approx_agent.train_utils import fit_models_no_augment, fit_models_augmented_data, \
-    events_from_state, LEARNING_RATE
+from agent_code.jr_approx_agent.callbacks import MODEL_FILE, POSSIBLE_ACTIONS
+import agent_code.jr_approx_agent.custom_events as ce
+from agent_code.jr_approx_agent.train_utils import fit_models_no_augment, fit_models_augmented_data, \
+    events_from_state, LEARNING_RATE, USE_DOUBLE_Q
 
 ROUNDS_TO_UPDATE_MODEL = 200
 
-USE_DATA_AUGMENTATION = True
+USE_DATA_AUGMENTATION = False
 
 GAME_REWARDS = {
-    e.MOVED_LEFT: 0.001,
-    e.MOVED_RIGHT: 0.001,
-    e.MOVED_UP: 0.001,
-    e.MOVED_DOWN: 0.001,
-    e.WAITED: -0.01,
-    e.INVALID_ACTION: -0.03,
+    e.MOVED_LEFT: 0,
+    e.MOVED_RIGHT: 0,
+    e.MOVED_UP: 0,
+    e.MOVED_DOWN: 0,
+    e.WAITED: -0.02,
+    e.INVALID_ACTION: -0.06,
     e.BOMB_EXPLODED: 0.002,
     e.BOMB_DROPPED: 0.0003,
     e.CRATE_DESTROYED: 0.05,
@@ -35,10 +34,15 @@ GAME_REWARDS = {
     e.KILLED_SELF: -0.3,
     e.GOT_KILLED: -0.1,
     e.OPPONENT_ELIMINATED: 0,
-    e.SURVIVED_ROUND: 0.1,
-    POTENTIAL_TO_COLLECT_COIN: 0.01,
-    POTENTIAL_TO_DIE_BY_BOMB: -0.01,
-    POTENTIAL_TO_DIE_BY_EXPLOSION: -0.01
+    e.SURVIVED_ROUND: 0.5,
+    ce.POTENTIAL_TO_COLLECT_COIN: 0.01,
+    ce.POTENTIAL_TO_NOT_COLLECT_COIN: -0.01,
+    ce.POTENTIAL_TO_DIE_BY_BOMB: -0.01,
+    ce.POTENTIAL_TO_NOT_DIE_BY_BOMB: 0.01,
+    ce.POTENTIAL_TO_DIE_BY_EXPLOSION: -0.01,
+    ce.POTENTIAL_TO_NOT_DIE_BY_EXPLOSION: 0.01,
+    ce.POTENTIAL_TO_EXPLODE_CRATE: 0.01,
+    ce.POTENTIAL_TO_NOT_EXPLODE_CRATE: -0.01
 }
 
 MODEL_FILE_TRAINING_A = 'weights_a.pt'
@@ -49,19 +53,25 @@ MODEL_FILE_TRAINING_TRACES = 'weights_traces.pt'
 def setup_training(self):
     self.logger.debug("Training setup")
 
+    feature_length = 253
     self.q_a_learn = MultiOutputRegressor(SGDRegressor(eta0=LEARNING_RATE), n_jobs=-1)
     self.q_a_predict = MultiOutputRegressor(SGDRegressor(eta0=LEARNING_RATE), n_jobs=-1)
 
-    self.q_b_learn = MultiOutputRegressor(SGDRegressor(eta0=LEARNING_RATE), n_jobs=-1)
-    self.q_b_predict = MultiOutputRegressor(SGDRegressor(eta0=LEARNING_RATE), n_jobs=-1)
+    self.q_a_learn.partial_fit([np.zeros(feature_length)], [np.zeros(len(POSSIBLE_ACTIONS))])
+    self.q_a_predict.partial_fit([np.zeros(feature_length)], [np.zeros(len(POSSIBLE_ACTIONS))])
 
-    self.q_a_learn.partial_fit([np.zeros(29)], [np.zeros(len(POSSIBLE_ACTIONS))])
-    self.q_a_predict.partial_fit([np.zeros(29)], [np.zeros(len(POSSIBLE_ACTIONS))])
-    self.q_b_learn.partial_fit([np.zeros(29)], [np.zeros(len(POSSIBLE_ACTIONS))])
-    self.q_b_predict.partial_fit([np.zeros(29)], [np.zeros(len(POSSIBLE_ACTIONS))])
+    if USE_DOUBLE_Q:
+        self.q_b_learn = MultiOutputRegressor(SGDRegressor(eta0=LEARNING_RATE), n_jobs=-1)
+        self.q_b_predict = MultiOutputRegressor(SGDRegressor(eta0=LEARNING_RATE), n_jobs=-1)
+
+        self.q_b_learn.partial_fit([np.zeros(feature_length)], [np.zeros(len(POSSIBLE_ACTIONS))])
+        self.q_b_predict.partial_fit([np.zeros(feature_length)], [np.zeros(len(POSSIBLE_ACTIONS))])
 
     self.learning_rate = LEARNING_RATE
-    self.scaler = StandardScaler()
+
+    self.eligibility_traces = {}
+    for action in POSSIBLE_ACTIONS:
+        self.eligibility_traces[action] = OrderedDict()
 
     self.round_count = 0
 
@@ -76,17 +86,21 @@ def game_events_occurred(self, old_game_state: dict, self_action: str, new_game_
     events = events_from_state(new_game_state, events)
     reward = reward_from_events(self, events)
 
-    fit_models(self, old_game_state, self_action, new_game_state, reward, self.last_act_exploration)
+    fit_models(self, old_game_state, self_action, new_game_state, reward)
 
     self.round_count += 1
     if self.round_count == ROUNDS_TO_UPDATE_MODEL:
         self.q_a_predict = None
         self.q_a_predict = deepcopy(self.q_a_learn)
 
-        self.q_b_predict = None
-        self.q_b_predict = deepcopy(self.q_b_learn)
+        if USE_DOUBLE_Q:
+            self.q_b_predict = None
+            self.q_b_predict = deepcopy(self.q_b_learn)
 
         self.round_count = 0
+
+        with open(MODEL_FILE, "wb") as file:
+            pickle.dump(self.q_a_predict, file)
 
 
 def end_of_round(self, last_game_state: dict, last_action: str, events: List[str]):
@@ -94,10 +108,7 @@ def end_of_round(self, last_game_state: dict, last_action: str, events: List[str
 
     reward = reward_from_events(self, events)
 
-    fit_models(self, last_game_state, last_action, None, reward, self.last_act_exploration)
-
-    with open(MODEL_FILE, "wb") as file:
-        pickle.dump(self.q_a_predict, file)
+    fit_models(self, last_game_state, last_action, None, reward)
 
 
 def reward_from_events(self, events: List[str]) -> int:
@@ -111,8 +122,8 @@ def reward_from_events(self, events: List[str]) -> int:
     return reward_sum
 
 
-def fit_models(self, old_game_state, action, new_game_state, reward, last_act_was_exploration):
+def fit_models(self, old_game_state, action, new_game_state, reward):
     if USE_DATA_AUGMENTATION:
-        fit_models_augmented_data(self, old_game_state, action, new_game_state, reward, last_act_was_exploration)
+        fit_models_augmented_data(self, old_game_state, action, new_game_state, reward)
     else:
-        fit_models_no_augment(self, old_game_state, action, new_game_state, reward, last_act_was_exploration)
+        fit_models_no_augment(self, old_game_state, action, new_game_state, reward)
